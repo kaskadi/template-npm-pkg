a8c558e230b1745e0cbd62a37f18a83510e6e91c
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-18 12:22:43 +0200

    Added JSDoc comments as example

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+/**
+ * You can use JSDoc comments in your `.js` files to generate automatically documentation for your package via the `generate-docs` workflow.
+ *
+ * @module help
+ */
+
 console.log('Hello World!')
