de82608c3f4bda871f0ea4f7584d07926f59a826
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-18 13:01:27 +0200

    Force docs generation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * You can use JSDoc comments in your `.js` files to generate automatically documentation for your package via the `generate-docs` workflow.
  *
  * @module help
+ *
  */
 
 console.log('Hello World!')
